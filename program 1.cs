7cec1cc11301c251eca798c6bef4ea4dad15a18c
lllppplll <onephoenixf@aol.com>
2020-06-06 13:36:39 +0100

    cs file

---
+++ program 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name");
+            string name = Console.ReadLine();
+            MyFunction(name);
+            
+            Console.ReadLine();
+        }
+    static void MyFunction(string a)
+    { 
+            Console.WriteLine("hello " + a);
+        }
+}
+}
